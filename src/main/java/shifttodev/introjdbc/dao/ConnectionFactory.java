bb6c8e26db0fd4f04037dc0edf9f7b54731fa72a
Luciano <lucianodacunha.net@gmail.com>
2023-11-26 02:50:10 -0300

    Configuração da lib dotenv-java.

--- src/main/java/shifttodev/introjdbc/dao/ConnectionFactory.java
+++ src/main/java/shifttodev/introjdbc/dao/ConnectionFactory.java
@@ -1,14 +1,19 @@
 package shifttodev.introjdbc.dao;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.*;
 
 public class ConnectionFactory {
     public static Connection getConnection(){
+        Dotenv dotenv = Dotenv.load();
+        String url = dotenv.get("DB_URL");
+        String user = dotenv.get("DB_USER");
+        String passwd = dotenv.get("DB_PASSWORD");
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            return DriverManager.getConnection("jdbc:mysql://192.168.0.110/livraria",
-                    "root", "senha");
+            return DriverManager.getConnection(url, user, passwd);
         } catch (SQLException | IllegalAccessException | InstantiationException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
